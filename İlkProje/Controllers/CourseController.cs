e84d31e4747c3ec48bac23148ac7cd778349ece2
mburaak <muhsin.demirtas2@gmail.com>
2023-09-07 14:22:40 +0300

    Tamamlandı

---
+++ İlkProje/Controllers/CourseController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using İlkProje.Models;
+
+namespace İlkProje.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm]Candidate model)
+        {
+            if(Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "There is already application.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            return View();
+        }
+    }
+}
